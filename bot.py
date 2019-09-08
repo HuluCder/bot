d0a966c503539ae5ee5256161ae947017c447890
HuluCder <37752535+HuluCder@users.noreply.github.com>
2019-09-08 22:41:02 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,12 +7,11 @@ bot = telebot.TeleBot('%925126748:AAGPjM_eMMPXpBLQ7a-EYDNwPYUNo40vHsc%');
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-
-if message.text == "Привет":
-    bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-elif message.text == "/help":
-    bot.send_message(message.from_user.id, "Напиши привет")
-else:
-    bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
 
 bot.polling(none_stop=True, interval=0)
